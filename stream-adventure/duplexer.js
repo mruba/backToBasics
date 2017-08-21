ff1fa087049b5b48943d3636b85dd492cbde1dee
mruba <mrubalcava64@gmail.com>
2017-08-21 11:41:28 -0700

    combiner

--- stream-adventure/duplexer.js
+++ stream-adventure/duplexer.js
@@ -1,8 +1,19 @@
-var spawn = require('child_process').spawn;
-var duplexer2 = require('duplexer2')
-module.exports = function (cmd, args) {
-    // spawn the process and return a single stream
-    // joining together the stdin and stdout here
-    var ps = spawn(cmd, args);
-    return duplexer(ps.stdin, ps.stdout)
+// var spawn = require('child_process').spawn;
+// var duplexer2 = require('duplexer2')
+// module.exports = function (cmd, args) {
+//     // spawn the process and return a single stream
+//     // joining together the stdin and stdout here
+//     var ps = spawn(cmd, args);
+//     return duplexer(ps.stdin, ps.stdout)
+// }
+
+
+var Button = function(content) {
+  this.content = content;
+};
+Button.prototype.click = function() {
+  console.log(this.content + ' clicked');
 }
+
+var myButton = new Button('OK');
+myButton.click();
